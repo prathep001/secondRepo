41d739cd57b6af6a208e949791fe31f6ef07ed69
Prathep <prathep.pathmanabhan.ext@siemens.com>
2022-07-13 16:10:53 +0530

    commitCheck4

--- TestScripts/testScript.m
+++ TestScripts/testScript.m
@@ -7,38 +7,38 @@ testScriptFilepath = mfilename('fullpath');
 modelDir= [parentDir '\ModelFile'];
 testDataDir = [parentDir '\TestData'];
 addpath(modelDir);
-fprintf(modelDir);
+disp(modelDir);
 addpath(testDataDir);
-fprintf(testDataDir);
+disp(testDataDir);
 % Searching Test Data Files.
 testFiles = dir([testDataDir '\*.mat']);
 if isempty(testFiles)
-    fprintf('No TestFiles are found');
+    disp('No TestFiles are found');
     return;
 end
 % Loading Model
-fprintf('Loading system model ....');
+disp('Loading system model ....');
 load_system(modelName)
-fprintf('Model loaded successfully');
+disp('Model loaded successfully');
 passCount = 0;
 failCount = 0;
 % Test file in loop.
 for idx = 1:length(testFiles)
-    fprintf('Clearing Test Data if any');
+    disp('Clearing Test Data if any');
     clear(inputName);
     testData = load(testFiles(idx).name);
     testName = fieldnames(testData);
-    fprintf('******************************************************************');
-    fprintf(['Test no: ' num2str(idx) ' - ' testName{1}]);
-    fprintf('******************************************************************');
+    disp('******************************************************************');
+    disp(['Test no: ' num2str(idx) ' - ' testName{1}]);
+    disp('******************************************************************');
     assignin('base',inputName,testData.(testName{1}));
-    fprintf('Simulating the model');
+    disp('Simulating the model');
     try
         sim(modelName);
-        fprintf('Test Passed');
+        disp('Test Passed');
         passCount = passCount+ 1;
     catch
-        fprintf('Test Failed');
+        disp('Test Failed');
         failCount = failCount+ 1;
         continue;
     end
@@ -46,14 +46,14 @@ end
 % Close system
 close_system(modelName,0);
 % Messages
-fprintf('******************************************************************');
-fprintf('Test Summary');
-fprintf('******************************************************************');
-fprintf(['Number of Test Cases: ' num2str(idx)]);
-fprintf(['Number of test Cases Passed: ' num2str(passCount)]);
-fprintf(['Number of test Cases Failed: ' num2str(failCount)]);
+disp('******************************************************************');
+disp('Test Summary');
+disp('******************************************************************');
+disp(['Number of Test Cases: ' num2str(idx)]);
+disp(['Number of test Cases Passed: ' num2str(passCount)]);
+disp(['Number of test Cases Failed: ' num2str(failCount)]);
 if ~isequal(failCount,0)
-    fprintf(errorMsg);
+    disp(errorMsg);
 else
-    fprintf('Overall Test Result: Passed');
+    disp('Overall Test Result: Passed');
 end
